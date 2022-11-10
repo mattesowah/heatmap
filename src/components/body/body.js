f763ffae8162c60da34f7de83517d4af64fa81f9
a17labs <*a17matte@gmail.com*>
2022-11-10 21:09:40 +0000

    changes

---
+++ src/components/body/body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landingpages from "./landingpage/landingpage";
+
+const body = () => {
+    return (
+      <div>
+        <Landingpages />
+      </div>
+      );
+}
+
+export default body
---
+++ src/components/body/landingpage/landingpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './landingpage.css'
+
+const landingpage = () => {
+
+  
+
+  return (
+    <div align="center" className='sectionstyle'>
+      <video   loop={true} data-autoplay data-keepplaying  autoPlay muted  className='renders' src="/assets/a.mp4" type="video./mp4" />
+    </div>
+  )
+}
+
+export default landingpage
